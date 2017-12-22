6e1f714b3a961853ca23533c6755647ce10e3b14
Dino Chan <dino.chan@live.com>
2017-12-22 14:44:59 +0800

    sync code...

---
+++ FluentDesignSystemSample/FluentDesignSystemSample/Models/AcrylicModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+
+namespace FluentDesignSystemSample.Models
+{
+  public  class AcrylicModel: DependencyObject
+    {
+        public string Name { get; set; }
+
+
+        /// <summary>
+        /// 获取或设置Brush的值
+        /// </summary>  
+        public AcrylicBrush Brush
+        {
+            get { return (AcrylicBrush)GetValue(BrushProperty); }
+            set { SetValue(BrushProperty, value); }
+        }
+
+        /// <summary>
+        /// 标识 Brush 依赖属性。
+        /// </summary>
+        public static readonly DependencyProperty BrushProperty =
+            DependencyProperty.Register("Brush", typeof(AcrylicBrush), typeof(AcrylicModel), new PropertyMetadata(null));
+    }
+}
--- FluentDesignSystemSample/FluentDesignSystemSample/Views/MaterialPage.xaml.cs
+++ FluentDesignSystemSample/FluentDesignSystemSample/Views/MaterialPage.xaml.cs
@@ -25,6 +25,7 @@ namespace FluentDesignSystemSample.Views
         public MaterialPage()
         {
             this.InitializeComponent();
+            CollectionViewSource
         }
     }
 }
