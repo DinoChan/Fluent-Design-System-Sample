595886555fd7ee1a8752c148cecba41e1a0b9172
Dino Chan <dino.chan@live.com>
2017-12-25 17:13:53 +0800

    sync code...

---
+++ FluentDesignSystemSample/FluentDesignSystemSample/Views/ConnectedAnimations/ConnectedAnimations2ndPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace FluentDesignSystemSample.Views.ConnectedAnimations
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class ConnectedAnimations2ndPage : Page
+    {
+        private static List<Item> items;
+
+        public ConnectedAnimations2ndPage()
+        {
+            this.InitializeComponent(); if (items == null)
+            {
+                items = new List<Item>
+                {
+                    new Item() { Title = "Test 1" },
+                    new Item() { Title = "Test 2" },
+                    new Item() { Title = "Test 3" },
+                    new Item() { Title = "Test 4" },
+                    new Item() { Title = "Test 5" },
+                    new Item() { Title = "Test 6" },
+                    new Item() { Title = "Test 7" },
+                    new Item() { Title = "Test 8" },
+                    new Item() { Title = "Test 9" },
+                    new Item() { Title = "Test 10" },
+                    new Item() { Title = "Test 11" },
+                    new Item() { Title = "Test 12" },
+                    new Item() { Title = "Test 13" },
+                    new Item() { Title = "Test 14" },
+                    new Item() { Title = "Test 15" },
+                    new Item() { Title = "Test 16" },
+                    new Item() { Title = "Test 17" },
+                    new Item() { Title = "Test 18" }
+                };
+            }
+
+            listView.ItemsSource = items;
+        }
+
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Frame.Navigate(typeof(ConnectedAnimations3rdPage), e.ClickedItem);
+        }
+    }
+
+    public class Item
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ FluentDesignSystemSample/FluentDesignSystemSample/Views/ConnectedAnimations/ConnectedAnimations3rdPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace FluentDesignSystemSample.Views.ConnectedAnimations
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class ConnectedAnimations3rdPage : Page
+    {
+
+        public ConnectedAnimations3rdPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public Item Item { get; set; }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Item = e.Parameter as Item;
+            base.OnNavigatedTo(e);
+        }
+    }
+}
